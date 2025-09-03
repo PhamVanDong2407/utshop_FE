a119b7a372c6cca80953a8cdec8ebd9a0e0700b6
DongKey <dongkeyphe@gmail.com>
2025-09-03 20:55:27 +0700

    update cấu hình thư mục file

---
+++ lib/Global/app_color.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color transparent = const Color(0x00000000);
+
+  static Color main = const Color(0xFFFFFFFF);
+  static Color background = const Color(0xFFF6F8FB);
+  static Color thirdMain = const Color(0xFFFFC55A);
+  static Color primary = const Color.fromARGB(255, 68, 136, 225);
+  static Color secondary = const Color(0xFF67AE6E);
+
+  static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
+  static Color red = const Color(0xFFFF0000);
+  static Color grey = const Color(0xFF8496AC);
+  static Color text1 = const Color(0xFF29303C);
+  static Color textHint = const Color(0xFF788A9B);
+  static Color boder = const Color(0xFFECEFF3);
+}
